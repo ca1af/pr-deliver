e51633e31860262dfd05f89ee906287f9f0d20bc
DK <laancer4@gmail.com>
2024-08-03 14:17:11 +0900

    init

---
+++ src/main/java/com/example/githubprconsumer/GithubPrConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubprconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPrConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubPrConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubprconsumer/GithubPrConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubprconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubPrConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
