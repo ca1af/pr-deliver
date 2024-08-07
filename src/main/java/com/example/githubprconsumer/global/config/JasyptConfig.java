5d5e3856c9cfec3bc6bef865a1f1cd344f4c5c27
DK <laancer4@gmail.com>
2024-08-08 03:42:04 +0900

    feat : Jasypt 사용해서 양방향 암호화

---
+++ src/main/java/com/example/githubprconsumer/global/config/JasyptConfig.java
@@ -0,0 +1,24 @@
+package com.example.githubprconsumer.global.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String key;
+
+    @Bean
+    public PooledPBEStringEncryptor jasyptStringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setPoolSize(8);
+        encryptor.setPassword(key);
+
+        return encryptor;
+    }
+}
