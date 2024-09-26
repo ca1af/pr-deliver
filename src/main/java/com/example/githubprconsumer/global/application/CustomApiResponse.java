ac4c82dd690e532aba53039d1569b4c0d86ef5af
DK <laancer4@gmail.com>
2024-09-26 23:23:04 +0900

    fix : ApiResponse 이름 변경

---
+++ src/main/java/com/example/githubprconsumer/global/application/CustomApiResponse.java
@@ -0,0 +1,29 @@
+package com.example.githubprconsumer.global.application;
+
+import lombok.Getter;
+
+@Getter
+public class CustomApiResponse<T> {
+    private final String message;
+    private final String status;
+    private T data;
+
+    public CustomApiResponse(String message, String status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public CustomApiResponse(String message, String status, T data) {
+        this.message = message;
+        this.status = status;
+        this.data = data;
+    }
+
+    public static <T> CustomApiResponse<T> ofSuccess(T data) {
+        return new CustomApiResponse<>("정상적으로 처리되었습니다", "Success", data);
+    }
+
+    public static <T> CustomApiResponse<T> ofFail(String message) {
+        return new CustomApiResponse<>(message, "Fail");
+    }
+}
