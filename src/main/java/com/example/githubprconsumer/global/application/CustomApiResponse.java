c5d574f7a336582a4b524ecff9a48b255f12a1d0
DK <laancer4@gmail.com>
2024-09-26 23:24:02 +0900

    feat : 데이터가 없어도 성공응답데이터를 내려주도록 변경

--- src/main/java/com/example/githubprconsumer/global/application/CustomApiResponse.java
+++ src/main/java/com/example/githubprconsumer/global/application/CustomApiResponse.java
@@ -26,4 +26,8 @@ public class CustomApiResponse<T> {
     public static <T> CustomApiResponse<T> ofFail(String message) {
         return new CustomApiResponse<>(message, "Fail");
     }
+
+    public static CustomApiResponse<Void> ofSuccess(){
+        return new CustomApiResponse<>("정상적으로 처리되었습니다", "Success");
+    }
 }
