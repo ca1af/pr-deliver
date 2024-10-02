59f9ea104d2db0089b6a6bfa3effdbe4390c05ee
DK <laancer4@gmail.com>
2024-10-02 14:59:32 +0900

    fix : GET -> POST 맵핑으로 변경

--- src/main/java/com/example/githubprconsumer/global/auth/presentation/AuthController.java
+++ src/main/java/com/example/githubprconsumer/global/auth/presentation/AuthController.java
@@ -5,7 +5,7 @@ import com.example.githubprconsumer.global.auth.application.dto.TokenResponseDto
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +16,7 @@ public class AuthController {
 
     private final OAuth2Service oAuth2Service;
 
-    @GetMapping("/oauth2/success")
+    @PostMapping("/oauth2/success")
     @Operation(summary = "Oauth2 로그인을 수행한다")
     public TokenResponseDto oauth2Login(@RequestParam String code) {
         return oAuth2Service.login(code);
