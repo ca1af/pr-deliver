cab760111a7c3677d32f3b0500f48fa37417bc65
DK <laancer4@gmail.com>
2024-10-18 16:18:10 +0900

    fix : 유니크 설정

--- src/main/java/com/example/githubprconsumer/messenger/domain/Messenger.java
+++ src/main/java/com/example/githubprconsumer/messenger/domain/Messenger.java
@@ -29,7 +29,7 @@ public class Messenger {
     @Column(nullable = false)
     private String login;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String webhookUrl;
 
     @Column(nullable = false)
