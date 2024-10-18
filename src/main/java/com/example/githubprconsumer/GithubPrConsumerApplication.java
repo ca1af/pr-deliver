b71a55b1373daeab2c4168c27c4d059a3cc3a01c
DK <laancer4@gmail.com>
2024-10-18 15:50:45 +0900

    feat : Swagger Config

--- src/main/java/com/example/githubprconsumer/GithubPrConsumerApplication.java
+++ src/main/java/com/example/githubprconsumer/GithubPrConsumerApplication.java
@@ -1,11 +1,12 @@
 package com.example.githubprconsumer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+@OpenAPIDefinition(servers = {@Server(url = "https://pr-deliver.shop", description = "PR-Deliver")})
 public class GithubPrConsumerApplication {
 
     public static void main(String[] args) {
